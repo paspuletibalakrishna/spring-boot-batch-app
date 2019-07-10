3dcfe7fce5a57a43b4b4bdacccab6f319b3e29f3
PASPULETI BALAKRISHNA <paspuletibalakrishna@PASPULETIs-Air.lan>
2019-07-10 13:46:20 +1000

    =Initial Spring boot Batch Application setup

---
+++ src/test/java/com/bala/demo/batch/SpringBatchAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bala.demo.batch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBatchAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
